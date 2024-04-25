596275286c3366c78d29d103adb67b9ab0d13c10
Arbab Khan <148271563+Arbab24178@users.noreply.github.com>
2024-04-26 03:07:19 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+let todoList = [];
+let condition = true;
+console.log(chalk.bgGray("\n \t welcome to Khans - Todo-List Application"));
+while (condition) {
+    let addTask = await inquirer.prompt([
+        {
+            name: "task",
+            type: "input",
+            message: " Input Your New Task : "
+        },
+    ]);
+    todoList.push(addTask.task);
+    console.log(`${addTask.task} Task added in Todo-list successfully`);
+    let addMoreTask = await inquirer.prompt([
+        {
+            name: "addMore",
+            type: "confirm",
+            message: "Do you want to add more task ?",
+            default: "false"
+        },
+    ]);
+    condition = addMoreTask.addMore;
+}
+console.log("Your updated Todo-list:", todoList);
